61f1402ae2eb9ad562ac8ee8ff20391591a801b8
ChezyName <mineboy2468@gmail.com>
2022-09-11 19:37:08 -0500

    Finish Raycast CannonJS Car

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import './App.css'
 
 import * as THREE from 'three'
 import { createRoot } from 'react-dom/client'
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState, Suspense } from 'react'
 import { Canvas, useFrame, ThreeElements, useThree } from '@react-three/fiber'
-import { OrbitControls, Stats } from "@react-three/drei";
+import { OrbitControls, Stats, Environment } from "@react-three/drei";
+import { Physics, useCylinder, usePlane } from '@react-three/cannon'
 
 import Car from './components/Car'
 import { onKeyPressed, UpdateCarPosition, onKeyReleased, IInput } from './components/Keybinds'
@@ -39,14 +40,50 @@ function App() {
 
       <Stats />
       <OrbitControls/>
+      {/** 
       <gridHelper args={[500,500]}/>
       <axesHelper args={[2]} />
+      */}
       
       <ambientLight/>
       <pointLight position={[0,4,0]}/>
-      <Car input={inputButtons} UpdateCarPosition={updateInput} carPosition={carPosition}/>
+      
+      <Physics broadphase="SAP" allowSleep>
+        <Car input={inputButtons} UpdateCarPosition={updateInput} carPosition={carPosition}/>
+
+        <Plane rotation={[-Math.PI / 2, 0, 0]} userData={{ id: 'floor' }} />
+        <Pillar position={[-5, 2.5, -5]} userData={{ id: 'pillar-1' }} />
+        <Pillar position={[0, 2.5, -5]} userData={{ id: 'pillar-2' }} />
+        <Pillar position={[5, 2.5, -5]} userData={{ id: 'pillar-3' }} />
+      </Physics>
+
+      <Suspense fallback={null}>
+          <Environment preset="night" />
+      </Suspense>
     </Canvas>
   )
 }
 
 export default App
+
+function Plane(props:any) {
+  const [ref] = usePlane<any>(() => ({ type: 'Static', material: 'ground', ...props }))
+  return (
+    <group ref={ref}>
+      <mesh receiveShadow>
+        <planeGeometry args={[100, 100]} />
+        <meshStandardMaterial color="#303030" />
+      </mesh>
+    </group>
+  )
+}
+
+function Pillar({ args = [0.7, 0.7, 5, 16], ...props }:any) {
+  const [ref] = useCylinder<any>(() => ({ mass: 10, args, ...props }))
+  return (
+    <mesh ref={ref} castShadow>
+      <cylinderGeometry args={args} />
+      <meshBasicMaterial color="#882929" />
+    </mesh>
+  )
+}
--- src/components/Car.tsx
+++ src/components/Car.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import React, { useRef, useState } from 'react'
 import { Canvas, useFrame, ThreeElements, useThree } from '@react-three/fiber'
 import {IInput} from "./Keybinds";
-import { clamp } from 'three/src/math/MathUtils';
+import { useRaycastVehicle, RaycastVehiclePublicApi } from '@react-three/cannon'
 
 type CarProps = {
     carPosition: THREE.Vector3,
@@ -12,73 +12,106 @@ type CarProps = {
 }
 
 const Car = ({carPosition, UpdateCarPosition, input}:CarProps) => {
-    const [carPos,setCarPos] = useState(new THREE.Vector3(0,0.25,0));
-    const bWheel = useRef<any>();
-    const fWheel = useRef<any>();
-    const fWheelR = useRef<any>();
+    const [carPos,setCarPos] = useState(new THREE.Vector3(0,0.4,0));
+
+    const chassis = useRef<any>()
+    const fr = useRef<any>()
+    const fl = useRef<any>()
+    const br = useRef<any>()
+    const bl = useRef<any>()
+
+    const api:RaycastVehiclePublicApi = createCar(1,1,2,3,-3,chassis,fr,fl,br,bl);
 
     useFrame(({ mouse }) => {
         //console.log(mouse);
         //UpdateCarPosition();
         //setCarPos(new THREE.Vector3(carPosition.x,0.35,carPosition.z));
 
-        let wheelRotation = fWheelR.current.rotation.y;
-        if(input.a){
-            wheelRotation += 0.05;
-        }
-        if(input.d){
-            wheelRotation -= 0.05;
-        }
-        if((input.a && input.d) || (input.a == false && input.d == false)){
-            if(wheelRotation > 0) wheelRotation -= 0.05;
-            if(wheelRotation < 0) wheelRotation += 0.05;
-        }
-
-        if(wheelRotation >= 0.25) wheelRotation = 0.25;
-        if(wheelRotation <= -0.25) wheelRotation = -0.25;
-        fWheelR.current.rotation.y = wheelRotation;
-
+        let speed = 0;
         if(input.w){
-            bWheel.current.rotation.x -= 0.1;
-            fWheel.current.rotation.x -= 0.1;
-
-            console.log(fWheelR.current.getWorldPosition());
+            //bWheel.current.rotation.x -= 0.1;
+            //fWheel.current.rotation.x -= 0.1;
+            speed++;
         }
         if(input.s){
-            bWheel.current.rotation.x += 0.1;
-            fWheel.current.rotation.x += 0.1;
+            //bWheel.current.rotation.x += 0.1;
+            //fWheel.current.rotation.x += 0.1;
+            speed--;
         }
     });
 
     useThree(({camera}) => {
-        //camera.position.set(carPosition.x,8,carPosition.z);
+        let ncp:THREE.Vector3 = camera.position.lerp(new THREE.Vector3(carPosition.x,carPosition.y + 8,carPosition.z),0.25);
+        camera.position.set(ncp.x,ncp.y,ncp.z);
     });
 
-  return (
-    <group position={carPos}>
-        <mesh name="body">
-            <meshBasicMaterial color="#0011ff"/>
-            <boxGeometry args={[1.5,0.75,3]}/>
-        </mesh>
+    return (
+        <group position={carPos}>
+            <mesh name="body">
+                <meshBasicMaterial color="#0011ff"/>
+                <boxGeometry args={[1.5,0.75,3]}/>
+            </mesh>
 
-        <mesh name="head" position={[0,0.5,0.3]}>
-            <meshBasicMaterial color="#00c3ff"/>
-            <boxGeometry args={[1.25,0.75,1.4]}/>
-        </mesh>
+            <mesh name="head" position={[0,0.5,0.3]}>
+                <meshBasicMaterial color="#00c3ff"/>
+                <boxGeometry args={[1.25,0.75,1.4]}/>
+            </mesh>
 
-        <mesh name="bWheel" position={[0,-0.1,0.9]} ref={bWheel}>
-            <meshBasicMaterial color="#444"/>
-            <boxGeometry args={[2,0.5,0.5]}/>
-        </mesh>
-
-        <group name="fWheelRot" position={[0,-0.1,-0.9]} ref={fWheelR}>
-            <mesh name="fWheel" ref={fWheel}>
+            <mesh name={"fr"} position={[0.8,-0.12,-1]} ref={fr}>
                 <meshBasicMaterial color="#444"/>
-                <boxGeometry args={[2,0.5,0.5]}/>
+                <boxGeometry args={[0.5,0.5,0.5]}/>
+            </mesh>
+
+            <mesh name={'fl'} position={[-0.8,-0.12,-1]} ref={fl}>
+                <meshBasicMaterial color="#444"/>
+                <boxGeometry args={[0.5,0.5,0.5]}/>
+            </mesh>
+
+            <mesh name={"br"} position={[0.8,-0.12,1]} ref={br}>
+                <meshBasicMaterial color="#444"/>
+                <boxGeometry args={[0.5,0.5,0.5]}/>
+            </mesh>
+
+            <mesh name={'bl'} position={[-0.8,-0.12,1]} ref={bl}>
+                <meshBasicMaterial color="#444"/>
+                <boxGeometry args={[0.5,0.5,0.5]}/>
             </mesh>
         </group>
-    </group>
-  )
+    )
 }
 
 export default Car
+
+function createCar(radius:number,width:number,height:number,front:number,back:number,chassis:any,wheel1:any,wheel2:any,wheel3:any,wheel4:any) {
+    const wheelInfo = {
+    radius,
+    directionLocal: [0, -1, 0],
+    suspensionStiffness: 30,
+    suspensionRestLength: 0.3,
+    maxSuspensionForce: 1e4,
+    maxSuspensionTravel: 0.3,
+    dampingRelaxation: 10,
+    dampingCompression: 4.4,
+    axleLocal: [-1, 0, 0],
+    chassisConnectionPointLocal: [1, 0, 1],
+    useCustomSlidingRotationalSpeed: true,
+    customSlidingRotationalSpeed: -30,
+    frictionSlip: 2
+    }
+
+    const wheelInfo1:any = { ...wheelInfo, isFrontWheel: true, chassisConnectionPointLocal: [-width / 2, height, front] }
+    const wheelInfo2:any = { ...wheelInfo, isFrontWheel: true, chassisConnectionPointLocal: [width / 2, height, front] }
+    const wheelInfo3:any = { ...wheelInfo, isFrontWheel: false, chassisConnectionPointLocal: [-width / 2, height, back] }
+    const wheelInfo4:any = { ...wheelInfo, isFrontWheel: false, chassisConnectionPointLocal: [width / 2, height, back] }
+
+    const [vehicle, api] = useRaycastVehicle(() => ({
+        chassisBody: chassis,
+        wheels: [wheel1, wheel2, wheel3, wheel4],
+        wheelInfos: [wheelInfo1, wheelInfo2, wheelInfo3, wheelInfo4],
+        indexForwardAxis: 2,
+        indexRightAxis: 0,
+        indexUpAxis: 1
+    }))
+
+    return api;
+}
